09f60fd39e819bf2632de335c147f3a47bb76907
Nguyễn Minh Đức <114391544+110100100100@users.noreply.github.com>
2024-04-22 09:33:44 +0700

    Add files via upload

---
+++ LearnAboutStaticMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿class Student
+{
+    private int rollno { get; set; }
+    private string name { get; set; }
+    private static string college = "BBDIT";
+    public Student(int r, string n)
+    {
+        rollno = r;
+        name = n;
+    }
+    public static void Change()
+    {
+        college = "CODEGYM";
+    }
+    public void Display()
+    {
+        System.Console.WriteLine(rollno + " " + name + " " + college);
+    }
+}
+
+ public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student.Change();
+            Student student1 = new Student(1, "Duck");
+            Student student2 = new Student(2, "Tung");
+            Student student3 = new Student(3, "Hung");
+           
+            student1.Display();
+            student2.Display();
+            student3.Display();
+        }
+    }
